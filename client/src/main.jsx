8901f2ec4d01bf9d37559c5d4caa254869d23177
nashira26 <fathimanashira@gmail.com>
2024-04-08 22:33:28 +0530

    Save user locally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
+import { persistor, store } from './redux/store.js'
 import { Provider } from 'react-redux'
-import {store} from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
+  <PersistGate persistor={persistor} loading={null}>
+    <App /> 
+  </PersistGate>
+</Provider>,
 )
